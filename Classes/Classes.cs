6fb27e90ffee3ccb3e4d0e826dcb924725830a3e
artemgon <artemhoncharuk123@gmail.com>
2025-01-11 09:32:45 +0200

    Task 2 is done.

--- Classes/Classes.cs
+++ Classes/Classes.cs
@@ -1,7 +1,58 @@
 ﻿
 namespace project_26_11_24.Classes
 {
-    internal class Classes
+    public class Shop
     {
+        public string? Title { get; set; }
+        public string? Address { get; set; }
+        public string? Description { get; set; }
+        public string? Phone { get; set; }
+        public string? Email { get; set; }
+        public float Area { get; set; }
+        public static float operator +(Shop shop1, float area)
+        {
+            return shop1.Area + area;
+        }
+        public static float operator -(Shop shop1, float area)
+        {
+            return shop1.Area - area;
+        }
+        public static bool operator ==(Shop shop1, Shop shop2)
+        {
+            return shop1.Area == shop2.Area;
+        }
+        public static bool operator >(Shop shop1, Shop shop2)
+        {
+            return shop1.Area > shop2.Area;
+        }
+        public static bool operator <(Shop shop1, Shop shop2)
+        {
+            return shop1.Area < shop2.Area;
+        }
+        public static bool operator !=(Shop shop1, Shop shop2)
+        {
+            return shop1.Area != shop2.Area;
+        }
+        public override bool Equals(object? obj)
+        {
+            if (obj is Shop otherShop)
+            {
+                return this.Area == otherShop.Area;
+            }
+            return false;
+        }
+        public override int GetHashCode()
+        {
+            return Area.GetHashCode();
+        }
+        public void PrintInfo()
+        {
+            Console.WriteLine("Shop Info:");
+            Console.WriteLine($"Title: {Title}");
+            Console.WriteLine($"Address: {Address}");
+            Console.WriteLine($"Description: {Description}");
+            Console.WriteLine($"Phone: {Phone}");
+            Console.WriteLine($"Email: {Email}");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -9,6 +9,33 @@ namespace project_26_11_24
             Console.ForegroundColor = ConsoleColor.DarkRed;
             try
             {
+                Shop shop1 = new Shop
+                {
+                    Title = "Shop1",
+                    Address = "Address1",
+                    Description = "Description1",
+                    Phone = "Phone1",
+                    Email = "Email1",
+                    Area = 100
+                };
+                Shop shop2 = new Shop
+                {
+                    Title = "Shop2",
+                    Address = "Address2",
+                    Description = "Description2",
+                    Phone = "Phone2",
+                    Email = "Email2",
+                    Area = 200
+                };
+                Console.WriteLine(shop1 + 50);
+                Console.WriteLine(shop1 - 50);
+                Console.WriteLine(shop1 == shop2);
+                Console.WriteLine(shop1 > shop2);
+                Console.WriteLine(shop1 < shop2);
+                Console.WriteLine(shop1 != shop2);
+                Console.WriteLine(shop1.Equals(shop2));
+                Console.WriteLine(shop1.GetHashCode());
+                shop1.PrintInfo();
             }
             catch (Exception e)
             {

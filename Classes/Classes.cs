9ae1ade6a401ac4fa098573cedac737f8499b66c
artemgon <artemhoncharuk123@gmail.com>
2025-01-11 09:19:08 +0200

    Task 1 is done.

--- Classes/Classes.cs
+++ Classes/Classes.cs
@@ -1,7 +1,63 @@
 ﻿
 namespace project_26_11_24.Classes
 {
-    internal class Classes
+    public class Magazine
     {
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? ContactPhone { get; set; }
+        public string? Email { get; set; }
+        public int CreationYear { get; set; }
+        public int StaffCount { get; set; }
+        public static Magazine operator +(Magazine magazine, int staffCount)
+        {
+            magazine.StaffCount += staffCount;
+            return magazine;
+        }
+        public static Magazine operator -(Magazine magazine, int staffCount)
+        {
+            magazine.StaffCount -= staffCount;
+            return magazine;
+        }
+        public static bool operator ==(Magazine magazine1, Magazine magazine2)
+        {
+            return magazine1.StaffCount == magazine2.StaffCount;
+        }
+        public static bool operator >(Magazine magazine1, Magazine magazine2)
+        {
+            return magazine1.StaffCount > magazine2.StaffCount;
+        }
+        public static bool operator <(Magazine magazine1, Magazine magazine2)
+        {
+            return magazine1.StaffCount < magazine2.StaffCount;
+        }
+        public static bool operator !=(Magazine magazine1, Magazine magazine2)
+        {
+            return magazine1.StaffCount != magazine2.StaffCount;
+        }
+        public override bool Equals(object? obj)
+        {
+            if (obj is Magazine otherMagazine)
+            {
+                return this.StaffCount == otherMagazine.StaffCount;
+            }
+            return false;
+        }
+        public override int GetHashCode()
+        {
+            return StaffCount.GetHashCode();
+        }
+        public void PrintInfo()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Magazine Info:");
+            Console.WriteLine("Title: " + Title);
+            Console.WriteLine("Description: " + Description);
+            Console.WriteLine("Contact Phone: " + ContactPhone);
+            Console.WriteLine("Email: " + Email);
+            Console.WriteLine("Creation Year: " + CreationYear);
+            Console.WriteLine("Staff Count: " + StaffCount);
+            Console.WriteLine();
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -9,6 +9,39 @@ namespace project_26_11_24
             Console.ForegroundColor = ConsoleColor.DarkRed;
             try
             {
+                Magazine magazine = new()
+                {
+                    Title = "Magazine Title",
+                    Description = "Magazine Description",
+                    ContactPhone = "123456789",
+                    Email = "magazineemail.com",
+                    CreationYear = 2020,
+                    StaffCount = 5
+                };
+                magazine.PrintInfo();
+                magazine += 5;
+                magazine.PrintInfo();
+                magazine -= 2;
+                magazine.PrintInfo();
+                Magazine magazine2 = new()
+                {
+                    Title = "Magazine Title 2",
+                    Description = "Magazine Description 2",
+                    ContactPhone = "987654321",
+                    Email = "magazineemail2.com",
+                    CreationYear = 2021,
+                    StaffCount = 8
+                };
+                magazine2.PrintInfo();
+                Console.WriteLine("magazine == magazine2: " + (magazine == magazine2));
+                Console.WriteLine("magazine > magazine2: " + (magazine > magazine2));
+                Console.WriteLine("magazine < magazine2: " + (magazine < magazine2));
+                Console.WriteLine("magazine != magazine2: " + (magazine != magazine2));
+                Console.WriteLine("magazine.GetHashCode(): " + magazine.GetHashCode());
+                Console.WriteLine("magazine2.GetHashCode(): " + magazine2.GetHashCode());
+                Console.WriteLine("magazine.Equals(magazine2): " + magazine.Equals(magazine2));
+                Console.WriteLine("magazine.Equals(magazine): " + magazine.Equals(magazine));
+                Console.WriteLine();
             }
             catch (Exception e)
             {

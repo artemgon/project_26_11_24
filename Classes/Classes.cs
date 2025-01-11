74169cbf34f54a4feefdc35b130675436ab8e7ad
artemgon <artemhoncharuk123@gmail.com>
2025-01-11 10:11:36 +0200

    Task 3 is done.

--- Classes/Classes.cs
+++ Classes/Classes.cs
@@ -1,7 +1,80 @@
 ﻿
 namespace project_26_11_24.Classes
 {
-    internal class Classes
+    internal class Books
     {
+        private readonly string?[] books = new string?[10];
+        public string? this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= books.Length)
+                {
+                    throw new IndexOutOfRangeException("Index out of range");
+                }
+                return books[index];
+            }
+            set
+            {
+                if (index < 0 || index >= books.Length)
+                {
+                    throw new IndexOutOfRangeException("Index out of range");
+                }
+                if (value == null || value == string.Empty)
+                {
+                    books[index] = null;
+                }
+                else
+                {
+                    books[index] = value;
+                }
+            }
+        }
+        public string TenthBook
+        {
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Book name cannot be null or empty.");
+                }
+                books[9] = value;
+            }
+        }
+        public bool IsAvailable(string book)
+        {
+            foreach (var item in books)
+            {
+                if (item == book)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static Books operator +(Books books, string book)
+        {
+            for (int i = 0; i < books.books.Length; i++)
+            {
+                if (books.books[i] == null)
+                {
+                    books.books[i] = book;
+                    return books;
+                }
+            }
+            return books;
+        }
+        public static Books operator -(Books books, string book)
+        {
+            for (int i = 0; i < books.books.Length; i++)
+            {
+                if (books.books[i] == book)
+                {
+                    books.books[i] = null;
+                    return books;
+                }
+            }
+            return books;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using project_26_11_24.Classes;
+﻿using System;
+using project_26_11_24.Classes;
 
 namespace project_26_11_24
 {
@@ -9,6 +10,29 @@ namespace project_26_11_24
             Console.ForegroundColor = ConsoleColor.DarkRed;
             try
             {
+                Books books = new Books
+                {
+                    [0] = "Book 1",
+                    [1] = "Book 2",
+                    [2] = "Book 3",
+                    [3] = "Book 4",
+                    [4] = "Book 5",
+                };
+                Console.WriteLine(books[0]);
+                Console.WriteLine(books[1]);
+                Console.WriteLine(books[2]);
+                Console.WriteLine(books[3]);
+                Console.WriteLine(books[4]);
+                books.TenthBook = "Book 10";
+                Console.WriteLine(books[9]); // tenth book is inaccessable using property
+                Console.WriteLine(books.IsAvailable("Book 1"));
+                Console.WriteLine(books.IsAvailable("Book 6"));
+                books = books + "Book 6";
+                Console.WriteLine(books.IsAvailable("Book 6"));
+                Console.WriteLine(books[6]);
+                books = books - "Book 6";
+                Console.WriteLine(books.IsAvailable("Book 6"));
+                Console.WriteLine();
             }
             catch (Exception e)
             {
